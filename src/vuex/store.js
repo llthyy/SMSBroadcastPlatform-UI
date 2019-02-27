8adbde1854247e636eb393f1ad5338831f6fd4bd
CSH6GIAWEAFSNDO\Administrator <583456429@qq.com>
2019-02-27 13:06:48 +0800

    修改

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -12,7 +12,7 @@ const mutations = {
     }
 }
 const state = {
-    information: {} || localStorage.getItem(JSON.parse(information)),
+    information: {} || localStorage.getItem(information),
 }
 
 const getters = {
